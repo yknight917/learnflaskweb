f1da761babfa1f6749a56b412bd5de720668e4c1
yyn <934325265@qq.com>
2018-07-11 16:21:57 +0800

    提交本地库

---
+++ flasky.py
@@ -0,0 +1,27 @@
+import os
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+from app.models import User, Role
+from flask_script import Manager, Shell
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
